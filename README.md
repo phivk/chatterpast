2e6f9a786a7cfb543a2c4226f0f3f5e54b7845dc
Philo van Kemenade <philovankemenade@gmail.com>
2022-06-20 23:43:48 +0200

    add files from tolerant-futures repo

---
+++ README.md
@@ -0,0 +1,3 @@
+# Tolerant Futures - Chatterpast
+
+A nonlinear chatbot story, powered by [Twine](https://twinery.org/) and the [Trialogue](https://github.com/phivk/trialogue) story format.
---
+++ story-javascript.js
@@ -0,0 +1,32 @@
+var leftSidebarContent       = `
+  <div class="text-center w-75">
+    <img class="mw-100 mb-4" src="https://raw.githubusercontent.com/phivk/tolerant-futures/chatterpast/assets/img/headshots.png" alt="chatterpast characters">
+    <h1>Chatterpast</h1>
+    <h4>
+      Tolerant Futures<br>
+      through<br>
+      Ancient Identities
+    </h4>
+  </div>`;
+var rightSidebarContent      = `
+  <p class="lead">Explore life in Iron Age and Roman Britannia with Enica and her friends.</p>
+  <p>Old lifeways are being challenged by the arrival of new people, new ideas, and new ways of living. Sharing experiences on Chatterpast has allowed a small group of kids to become friends, despite their differences. </p>
+  <p>Discover their unique stories from across Britannia. Question them about their lives and reflect on how these relate to the present.</p>
+  <p>The friends are also curious about how things turned out around 2000 years in their future!</p>`;
+var userResponseHintContent  = 'Choose one option to continue the conversation';
+var modalTitleContent        = 'Leave the chat?';
+var modalBodyContent         = '<p>If you leave the chat, your history will be lost and you will have to start from the beginning.</p>';
+var modalFooterContent       = '<button type="button" class="btn rounded-0 btn-link" data-dismiss="modal">Stay</button><a href="/?lang=en" type="button" class="btn rounded-0 btn-danger">Leave</a>';
+var navLinkBackContent       = '<img class="nav-icon nav-icon-back" src="https://fillafulla.sng.sk/img/icon-back.svg">';
+var navLinkMenuContent       = '<img class="nav-icon nav-icon-menu" src="https://fillafulla.sng.sk/img/icon-hamburger.svg">';
+
+inject_nav_back(navLinkBackContent);
+inject_nav_menu(navLinkMenuContent);
+inject_left_sidebar(leftSidebarContent);
+inject_right_sidebar(rightSidebarContent);
+inject_hint(userResponseHintContent);
+inject_modal(modalTitleContent, modalBodyContent, modalFooterContent);
+
+$(document).ready(function() {
+  fade_in_content_containers();
+});
